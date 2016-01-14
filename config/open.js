23d4c17f1fc63f29d98ab6bf83b242ca7d037faa
wugeng <baluwu.crap@gmail.com>
2016-01-14 18:12:10 +0800

    test jos send order

---
+++ config/open.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+    /* taobao */
+    TAOBAO_APPKEY: 'key',
+    TAOBAO_APPSECRET: 'secret',
+    TAOBAO_URL: 'http://gw.api.taobao.com/router/rest',
+
+    /* jos */
+    JOS_APPKEY: '',
+    JOS_APPSECRET: '',
+    JOS_URL: 'http://gw.api.jd.com/routerjson'
+};
