6e4014e4a33a19f9028f63296a3c415096fd2577
wugeng <baluwu.crap@gmail.com>
2016-01-15 17:06:47 +0800

    no repeat

---
+++ common/http/http.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var http = require('http')
+    , querystring = require('querystring')
+    , _ = require('underscore');
+
+/**
+ * HTTP Post请求
+ * @param host 主机名
+ * @param path 请求路径
+ * @param port 端口
+ * @param data {String} 请求的body参数 
+ * @param head {Object} 请求头 
+ * @param callback 回调函数
+ * @constructor
+ */
+var _HTTPPost = function (host, path, port, head, data, callback) {
+    
+    var result = ''
+        , post_data = data;
+
+    var options = {
+        hostname: host,
+        port: port || 80,
+        path: path || '/',
+        method: 'POST',
+        headers: _.extend(head, {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+            'Content-Length': post_data.length,
+            'timeout': 150000
+        })
+    };
+    
+    var req = http.request(options, function (res) {
+        res.setEncoding('utf8');
+
+        res.on('data', function (chunk) {
+            result += chunk;
+        });
+
+        res.on('end', function (chunk) {
+            callback(null, result);
+        });
+    });
+
+    req.on('error', function (e) {
+        console.log('problem with request: ' + e.message);
+        callback(e.message, null);
+    });
+
+    data.length > 0 && req.write(post_data + '\n');
+    req.end();
+};
+
+exports.request = _HTTPPost;
